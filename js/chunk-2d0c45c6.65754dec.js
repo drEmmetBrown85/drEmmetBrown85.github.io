caa7d7b806416a33cc92fe10f1930d1c9ede3bfa
Dr Brown <dr.emmet.jones@gmail.com>
2021-12-06 04:29:53 +0000

    inital

---
+++ js/chunk-2d0c45c6.65754dec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c45c6"],{"3b01":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an Roadmap page")])])}],s=t("2877"),u={},i=Object(s["a"])(u,a,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0c45c6.65754dec.js.map
